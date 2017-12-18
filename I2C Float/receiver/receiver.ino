c28bbfaf2a0e27abf6c918e3a6a1bd47b36e46bd
duartedornellas <duarte_dornellas@hotmail.com>
2017-12-18 12:32:19 +0000

    i2c float array

--- I2C Float/receiver/receiver.ino
+++ I2C Float/receiver/receiver.ino
@@ -2,37 +2,44 @@
 
 #include<Wire.h>
 
-int N_ELEMENTS = 2;
+const int N_ELEMENTS = 2;
 
-union float_as_bytes {byte b[4]; float fval;} u;
+union float_as_bytes {byte b[4]; float fval;};
 
+float_as_bytes u[N_ELEMENTS];
 
 void receiveEvent(int numBytes){
-    char data[4];
+    char data_receive[N_ELEMENTS][4];
     int i = 0;
-    while(Wire.available())    // slave may send less than requested
-   { 
-     data[i] = Wire.read(); // receive a byte as character 
-     Serial.print("B: ");
-     Serial.println(data[i]); 
-     i = i + 1;
-   }
-    if(i==4){
-        u.b[0] = data[0];    
-        u.b[1] = data[1];
-        u.b[2] = data[2];
-        u.b[3] = data[3];
+    while(Wire.available()){    // slave may send less than requested   
+        for(int j=0; j<4; j++){
+        data_receive[i][j] = Wire.read(); // receive a byte as character 
+        u[i].b[j] = data_receive[i][j];
+        Serial.print("B: ");
+        Serial.println(data_receive[i]); 
+        }
+    i++;
+    }
     
-        float aux = u.fval;
-        Serial.print("Pi = ");
+   if(i==2){      
+//u.b[0] = data_receive[0];    
+//u.b[1] = data_receive[1];
+//u.b[2] = data_receive[2];
+//u.b[3] = data_receive[3];
+        for(int i=0; i<N_ELEMENTS; i++){
+        float aux = u[i].fval;
+        Serial.print("float(");
+        Serial.print(i);
+        Serial.print(") = ");
         Serial.println(aux); 
+        }
     }
 }
 
 void setup() {
 Serial.begin(9600);
 
-Wire.begin(1);
+Wire.begin(8);
 Wire.onReceive(receiveEvent);
 }
 
--- I2C Float/transmitter/transmitter.ino
+++ I2C Float/transmitter/transmitter.ino
@@ -4,19 +4,17 @@
 
 int N_ELEMENTS = 2;
 
-union float_as_bytes {byte b[4]; float fval;};
-
 void setup() {
 Serial.begin(9600);
 Wire.begin();
 
-float pi = 3.14;
+float data_send[N_ELEMENTS] = {1.23, 4.56};
 
 int control = 1;
 while(control!=0){
     Serial.println("Trying...");
-    Wire.beginTransmission(1);
-    Wire.write((byte*) &pi,4);
+    Wire.beginTransmission(8);
+    Wire.write((byte*) &data_send, 4*N_ELEMENTS);
     control = Wire.endTransmission(); 
 }
 control = 1;
